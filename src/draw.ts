02f7f583a1afe6aac89e79034e7fc2ec7e2fa949
Michael Little <mike@icbmike.com>
2022-08-22 23:23:45 -0700

    draw grid

--- src/draw.ts
+++ src/draw.ts
@@ -4,10 +4,24 @@ export const draw = (ctx: CanvasRenderingContext2D, game: IGame, delta: number) 
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
     for (let i = 0; i < game.getSize(); i++) {
+
         for (let j = 0; j < game.getSize(); j++) {
             if (game.isAlive(i, j)) {
                 ctx.fillRect(i * 10, j * 10, 10, 10);
             }
         }
+
+        ctx.beginPath();
+        ctx.strokeStyle = 'grey';
+        ctx.moveTo((i + 1) * 10, 0);
+        ctx.lineTo((i + 1) * 10, ctx.canvas.height);
+        ctx.stroke();
+    }
+
+    for (let j = 0; j < game.getSize(); j++) {
+        ctx.beginPath();
+        ctx.moveTo(0, j * 10);
+        ctx.lineTo(ctx.canvas.width, j * 10);
+        ctx.stroke();
     }
 }
